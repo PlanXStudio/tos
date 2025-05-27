d43d452900d055b39a9293288c13e4e1339431ae
chanmin.park <8893544+PlanXStudio@users.noreply.github.com>
2025-05-27 09:42:43 +0900

    Update Microsoft.PowerShell_profile.ps1

--- win/pwsh7/Microsoft.PowerShell_profile.ps1
+++ win/pwsh7/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tos-term.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:LocalAppData\Programs\oh-my-posh\themes\tos-term.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module -Name modern-unix-win
 Import-Module -Name PSFzf
